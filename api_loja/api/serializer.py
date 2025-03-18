db18623f6a67c6462ad15252c013f2666595c446
heloisa <helomc210807@aluno.senai.br>
2025-03-18 11:19:15 -0300

    loja yaishe

--- api_loja/api/serializer.py
+++ api_loja/api/serializer.py
@@ -5,18 +5,42 @@ class ClienteSerializer(serializers.ModelSerializer):
     class Meta: 
         model = models.Cliente
         fields = "__all__"
+        extra_kwargs={
+            'id' : {'help_text':'Identificador do cliente'},
+            'nome': {'help_text': 'Nome do cliente'}
+        }
 
 class MostruarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Mostruario
         fields = "__all__"
+        extra_kwargs={
+            'id' : {'help_text':'Identificador do mostruário'},
+            'nome': {'help_text': 'nome do produto'},
+            'DataAlt':{'help_text': 'data de alteração do mostruario'},
+            'Descricao':{'help_text': 'Descrição do que mostruario'},
+            'Foto': {'help_text': 'Foto da roupa'}
+        }
 
 class CarrinhoSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Carrinho 
         fields = "__all__"
+        extra_kwargs={
+            'id' : {'help_text':'Identificador do Carrinho'},
+            'Cliente':{'help_text': 'nome do cliente '},
+            'Data_Criacao':{'help_text':'Data da criação do carrinho'},
+            'Ativo':{'help_text': 'Se o carrinho está ativo ou foi finalizado'},
+        }
 
 class ItemCarrinhoSerializer(serializers.ModelSerializer):
     class Meta: 
         model = models.ItemCarrinho
         fields = "__all__"
+        extra_kwagers={
+            'id': {'help_text': 'Identificador do Item no carrinho'},
+            'Carrinho': {'help_text': 'Relaciona o item com o carrinho'},
+            'Produto': {'help_text': 'Item do monstruario'},
+            'Quantidade':{'help_text': 'Quantidade de produtos'},
+            'PrecoUnitario': {'help_text': 'Preço de cada produto unidade'},
+        }
--- api_loja/api/viewsets.py
+++ api_loja/api/viewsets.py
@@ -1,19 +1,141 @@
 from rest_framework import viewsets 
 from api_loja.api import serializer
 from api_loja import models 
+from drf_yasg.utils import swagger_auto_schema
+
 
 class ClienteViewsets(viewsets.ModelViewSet):
     serializer_class = serializer.ClienteSerializer
     queryset = models.Cliente.objects.all()
 
+    @swagger_auto_schema(
+        operation_description="Lista de todos os clientes",
+        responses={200: serializer.ClienteSerializer (many=True)}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    @swagger_auto_schema(
+        operation_description="Crie um novo cliente",
+        responses={201: "Cliente criado com sucesso"}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_description="Altera o cliente conforme ID e dados informados",
+        responses={200: "Cliente alterado com sucesso"}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_description="Deletar o cliente conforme ID e dados informados",
+        responses={200: "Cliente deletado com sucesso"}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    
+    
+
 class MostruarioViewsets(viewsets.ModelViewSet):
     serializer_class= serializer.MostruarioSerializer
     queryset = models.Mostruario.objects.all()
+   
+    @swagger_auto_schema(
+        operation_description="Listar todos os mostruários",
+        responses={200: serializer.MostruarioSerializer(many=True)}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    #metodo get
+
+    @swagger_auto_schema(
+        operation_description="Criar um novo mostruário",
+        responses={201:"Mostruário criado com sucesso"}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    #metodo post
+    
+    @swagger_auto_schema(
+        operation_description="Alterar o mostruario conforme ID e dados informados",
+        responses={200: "mostruario alterado com sucesso"}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_description="Deleta o mostruario confrome ID informado",
+        responses={204: "mostruario deletada com sucesso"}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+
+
 
 class CarrinhoViewsets(viewsets.ModelViewSet):
     serializer_class = serializer.CarrinhoSerializer
     queryset = models.Carrinho.objects.all()
 
+    @swagger_auto_schema(
+        operation_description="Listar carrinho",
+        responses={200: serializer.MostruarioSerializer(many=True)}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    #metodo get
+
+    @swagger_auto_schema(
+        operation_description="Adicionar ao carrinho",
+        responses={201:"Adicionado ao carrinho com sucesso"}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    #metodo post
+    
+    @swagger_auto_schema(
+        operation_description="Deletar carrinho",
+        responses={200: "Carrinho apagado com sucesso"}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    
+
+
+
+
 class ItemCarrinhoViewsets(viewsets.ModelViewSet):
     serializer_class = serializer.ItemCarrinhoSerializer
     queryset = models.ItemCarrinho.objects.all()
+
+    @swagger_auto_schema(
+        operation_description="Listar todos os itens do carrinho",
+        responses={200: serializer.CarrinhoSerializer(many=True)}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    #metodo get
+    @swagger_auto_schema(
+        operation_description="Adicionar um novo item",
+        responses={201:"Item adicionado com sucesso"}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    #metodo post
+    
+    @swagger_auto_schema(
+        operation_description="Altera item do carrinho",
+        responses={200: "Alteração feita com sucesso"}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    
+    
+    @swagger_auto_schema(
+        operation_description="Deleta o item do carrinho confrome ID informado",
+        responses={204: "Item deletado com sucesso"}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
